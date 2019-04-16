d6242083bc7c1f58da2ff8db924bfe30aba7cbe2
DannyHinshaw <danny@nulleffort.com>
2019-04-16 19:11:09 +0000

    Add debugging lines

--- base.sh
+++ base.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 # Clone repo and cleanup
+echo "Cloning repo..."
 git clone --depth=1 --branch=master https://github.com/DannyHinshaw/typescript-redux-setup.git
-rm -rf ./dirformynewrepo/.git
+
+echo "Delete typescript-redux-setup/.git"
+rm -rf ./typescript-redux-setup/.git
+
+echo "Moving up store module..."
 mv typescript-redux-setup/store ./
+
+echo "Removing typescript-redux-setup"
 rm -r typescript-redux-setup

9cb3f3a26005c86c9528111d34fe176ba87457b2
Danny Hinshaw <danny@nulleffort.com>
2019-04-16 23:52:59 +0000

    Remove @types/redux

--- npm.sh
+++ npm.sh
@@ -6,4 +6,4 @@ bash <(curl -s https://raw.githubusercontent.com/DannyHinshaw/typescript-redux-s
 # Install project dependencies
 echo "Installing npm packages..."
 npm i --save redux react-redux redux-logger redux-devtools-extension redux-persist redux-thunk typesafe-actions
-npm i --save-dev @types/redux @types/react-redux
+npm i --save-dev @types/react-redux

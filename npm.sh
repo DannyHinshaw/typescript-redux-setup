6bc9629c1ba394f54af30e4f604960c9dc930ba7
DannyHinshaw <danny@nulleffort.com>
2019-04-16 19:12:45 +0000

    Add debugging lines

--- npm.sh
+++ npm.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Calling base.sh"
 bash <(curl -s https://raw.githubusercontent.com/DannyHinshaw/typescript-redux-setup/master/base.sh)
 
 # Install project dependencies
+echo "Installing npm packages..."
 npm i --save redux react-redux redux-logger redux-devtools-extension redux-persist redux-thunk typesafe-actions
 npm i --save-dev @types/redux @types/react-redux
--- yarn.sh
+++ yarn.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Calling base.sh"
 bash <(curl -s https://raw.githubusercontent.com/DannyHinshaw/typescript-redux-setup/master/base.sh)
 
 # Install project dependencies
+echo "Installing npm packages..."
 yarn add redux react-redux redux-logger redux-devtools-extension redux-persist redux-thunk typesafe-actions
 yarn add --dev @types/redux @types/react-redux

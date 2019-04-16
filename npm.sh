4148b5a0a2c9f298cea19efa383ef2c86e29adcc
DannyHinshaw <danny@nulleffort.com>
2019-04-16 18:53:26 +0000

    Add base.sh to yarn/npm scripts

--- npm.sh
+++ npm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source base.sh
+
 npm i --save redux react-redux redux-logger redux-devtools-extension redux-persist redux-thunk typesafe-actions
 
 npm i --save-dev @types/redux @types/react-redux
--- yarn.sh
+++ yarn.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source base.sh
+
 yarn add redux react-redux redux-logger redux-devtools-extension redux-persist redux-thunk typesafe-actions
 
 yarn add --dev @types/redux @types/react-redux
